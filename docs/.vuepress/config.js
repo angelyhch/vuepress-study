9944ce4ee543d7edc883c8cc25714607f6ea9927
angelyhch <angelyhch@163.com>
2022-10-18 12:26:27 +0800

    配置导航和侧边栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: ' angel blog',
+  description: '学习vuepress的demo',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Python', link: '/languages/python/' },
+          { text: 'Java', link: '/languages/java/' },
+          { text: 'C++', link: '/languages/C++/' }
+        ]
+      },
+      { text: 'External', link: 'https://baidu.com' }
+    ],
+    sidebar: 'auto',
+    lastUpdated: true
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+# Hello VuePress
+
+## tt
+
+this.$site:
+
+{{this.$site}}
+
+<span v-for="i in 3"> {{i}}</span>
+
+this.$page:{{this.$page}}
+## index循环
+<p v-for="item,index in  $site"> {{item, index}}</p>
+
+## this.$route
+<!-- {{this.$router}} -->
